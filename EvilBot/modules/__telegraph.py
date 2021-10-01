6d92125a7a1a417e843e5d1a32732564e0859ea1
Black Storm Userbot <91207660+TeamBlackStorm@users.noreply.github.com>
2021-10-02 00:34:34 +0530

    Update __telegraph.py

--- EvilBot/modules/__telegraph.py
+++ EvilBot/modules/__telegraph.py
@@ -6,7 +6,7 @@ import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-shrvan42 = "EVIL"
+LEGENDXGAURAV = "EVIL"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=shrvan42)
 auth_url = r["auth_url"]
